db7e66adcec608a3eba3f081208e8e760461c7b6
David Lopes <david.mlopes15@gmail.com>
2024-01-09 02:06:12 +0000

    Better mapping AP to MAL

--- lib/mal.ts
+++ lib/mal.ts
@@ -1,6 +1,8 @@
 export async function getAnime(name: string) {
     const anime = await fetch(
-        'https://api.myanimelist.net/v2/anime?q=' + name,
+        'https://api.myanimelist.net/v2/anime?q=' +
+            name +
+            '&fields=alternative_titles',
         {
             method: 'GET',
             headers: {
@@ -14,12 +16,36 @@ export async function getAnime(name: string) {
         .then((data) => {
             let anime = 'NOT FOUND'
 
-            data.data.forEach((element: { node: { title: string } }) => {
-                if (element.node.title === name) {
-                    anime = element.node.title
-                    return
-                }
-            })
+            data.data.forEach(
+                (element: {
+                    node: {
+                        title: string
+                        alternative_titles: {
+                            synonyms: Array<string>
+                            en: string
+                        }
+                    }
+                }) => {
+                    if (element.node.title === name) {
+                        anime = element.node.title
+                        return
+                    }
+
+                    element.node.alternative_titles.synonyms.forEach(
+                        (synonym) => {
+                            if (synonym === name) {
+                                anime = element.node.title
+                                return
+                            }
+                        },
+                    )
+
+                    if (element.node.alternative_titles.en === name) {
+                        anime = element.node.title
+                        return
+                    }
+                },
+            )
 
             return anime
         })
