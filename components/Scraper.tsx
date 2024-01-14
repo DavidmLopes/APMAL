f33cc6223b7bc534e678c6c846e70fab9313559c
David Lopes <david.mlopes15@gmail.com>
2024-01-14 01:27:53 +0000

    Better map animes AP to MAL
    Scrape for more info;
    Multiple levels of mapping.

--- components/Scraper.tsx
+++ components/Scraper.tsx
@@ -1,14 +1,18 @@
 import { getAnime } from '@/lib/mal'
 import * as cheerio from 'cheerio'
 
-const AP_URL =
-    'https://www.anime-planet.com/users/Master15/anime?sort=title&mylist_view=grid&per_page=560'
+const AP_URL = 'https://www.anime-planet.com/users/Master15/anime?sort=title'
 
 export default async function Scraper() {
     async function scraperAction() {
         'use server'
 
-        const animes: string[] = []
+        const animes: Array<{
+            title: string
+            alternative_titles: string[]
+            year: string
+            total_eps: string
+        }> = []
 
         async function getAnimes(url: string) {
             const html = await fetch(url).then((res) => res.text())
@@ -20,7 +24,30 @@ export default async function Scraper() {
                 .each((i, el) => {
                     const title = $(el).find('h3').text()
 
-                    animes.push(title)
+                    const info = $(el).find('.tooltip').attr('title')
+                    let year = ''
+                    const alternative_titles = [] as string[]
+                    const total_eps = $(el).attr('data-total-episodes') ?? ''
+                    if (info) {
+                        const $$ = cheerio.load(info)
+                        year = $$('.iconYear').text().substring(0, 4)
+                        const alternative_title = $$('h6').text().substring(11)
+                        if (alternative_title != '') {
+                            if (alternative_title.charAt(0) === ' ') {
+                                alternative_title
+                                    .split(',')
+                                    .forEach((title) => {
+                                        alternative_titles.push(
+                                            title.substring(1),
+                                        )
+                                    })
+                            } else {
+                                alternative_titles.push(alternative_title)
+                            }
+                        }
+                    }
+
+                    animes.push({ title, alternative_titles, year, total_eps })
                 })
 
             if ($('.next a').length > 0) {
@@ -32,19 +59,39 @@ export default async function Scraper() {
         await getAnimes(AP_URL)
 
         const animesData = await Promise.all(
-            animes.map(async (title) => {
-                const anime = await getAnime(title)
+            animes.map(
+                async ({ title, alternative_titles, year, total_eps }) => {
+                    const anime = await getAnime(
+                        title,
+                        alternative_titles,
+                        year,
+                        total_eps,
+                    )
 
-                return { ap: title, mal: anime }
-            }),
+                    return { ap: title, mal: anime }
+                },
+            ),
         )
 
         const notfounds = animesData.filter(
-            (anime) => anime.mal === 'NOT FOUND' || anime.mal === 'ERROR',
+            (anime) => anime.mal.info === 'NOT FOUND',
         )
 
-        console.log(animesData.length)
-        console.log(notfounds.length)
+        const errors = animesData.filter((anime) => anime.mal.info === 'ERROR')
+
+        const extras = animesData.filter(
+            (anime) => anime.mal.info === 'FOUND WITH EXTRA PROPS',
+        )
+
+        const extras2 = animesData.filter(
+            (anime) => anime.mal.info === 'FOUND WITHOUT API',
+        )
+
+        console.log('TOTAL: ' + animesData.length)
+        console.log('FOUND WITH EXTRA PROPS: ' + extras.length)
+        console.log('FOUND WITHOUT API: ' + extras2.length)
+        console.log('NOT FOUND: ' + notfounds.length)
+        console.log('ERROR: ' + errors.length)
     }
 
     return (
--- lib/mal.ts
+++ lib/mal.ts
@@ -1,8 +1,28 @@
-export async function getAnime(name: string) {
-    const anime = await fetch(
+type MALNode = {
+    node: {
+        title: string
+        alternative_titles: {
+            synonyms: Array<string>
+            en: string
+        }
+        start_season: {
+            year: number
+            season: string
+        }
+        num_episodes: number
+    }
+}
+
+export async function getAnime(
+    name: string,
+    alternative_titles: string[] = [],
+    year: string = '0',
+    total_eps: string = '0',
+) {
+    const { anime, info } = await fetch(
         'https://api.myanimelist.net/v2/anime?q=' +
-            name +
-            '&fields=alternative_titles',
+            name.slice(0, 50) +
+            '&limit=20&fields=alternative_titles,start_season,num_episodes',
         {
             method: 'GET',
             headers: {
@@ -11,47 +31,89 @@ export async function getAnime(name: string) {
         },
     )
         .then((res) => {
+            // TODO: If 403, maybe something is wrong with the API key
+
             return res.json()
         })
-        .then((data) => {
-            let anime = 'NOT FOUND'
-
-            data.data.forEach(
-                (element: {
-                    node: {
-                        title: string
-                        alternative_titles: {
-                            synonyms: Array<string>
-                            en: string
-                        }
-                    }
-                }) => {
-                    if (element.node.title === name) {
-                        anime = element.node.title
-                        return
-                    }
-
-                    element.node.alternative_titles.synonyms.forEach(
-                        (synonym) => {
-                            if (synonym === name) {
-                                anime = element.node.title
-                                return
-                            }
-                        },
-                    )
-
-                    if (element.node.alternative_titles.en === name) {
-                        anime = element.node.title
-                        return
-                    }
-                },
+        .then(async (data) => {
+            const animeName = simpleTitle(name)
+            const animeAltTitles = alternative_titles.map((title) =>
+                simpleTitle(title),
             )
 
-            return anime
+            let anime: MALNode
+
+            anime = data.data.find(
+                (element: MALNode) =>
+                    simpleTitle(element.node.title) === animeName ||
+                    element.node.alternative_titles.synonyms
+                        .map((synonym) => simpleTitle(synonym))
+                        .includes(animeName) ||
+                    simpleTitle(element.node.alternative_titles.en) ===
+                        animeName ||
+                    animeAltTitles.includes(simpleTitle(element.node.title)) ||
+                    animeAltTitles.includes(
+                        simpleTitle(element.node.alternative_titles.en),
+                    ),
+            )
+
+            if (anime) {
+                return { anime: anime.node.title, info: 'FOUND' }
+            }
+
+            anime = data.data.find(
+                (element: MALNode) =>
+                    element.node.start_season != undefined &&
+                    year != '0' &&
+                    element.node.start_season.year === parseInt(year) &&
+                    total_eps != '0' &&
+                    element.node.num_episodes === parseInt(total_eps),
+            )
+
+            if (anime) {
+                return {
+                    anime: anime.node.title,
+                    info: 'FOUND WITH EXTRA PROPS',
+                }
+            }
+
+            const animeElem = await fetch(
+                'https://myanimelist.net/search/prefix.json?type=anime&keyword=' +
+                    name +
+                    '&v=1',
+            )
+                .then((res) => {
+                    return res.json()
+                })
+                .then((data) => {
+                    const animeElem: { name: string } =
+                        data.categories[0].items.find(
+                            (element: { name: string }) =>
+                                simpleTitle(element.name) === animeName ||
+                                animeAltTitles.includes(
+                                    simpleTitle(element.name),
+                                ),
+                        )
+
+                    return animeElem
+                })
+
+            if (animeElem) {
+                return {
+                    anime: animeElem.name,
+                    info: 'FOUND WITHOUT API',
+                }
+            }
+
+            return { anime: '', info: 'NOT FOUND' }
         })
         .catch((err) => {
-            return 'ERROR'
+            return { anime: '', info: 'ERROR' }
         })
 
-    return anime
+    return { anime, info }
+}
+
+function simpleTitle(title: string) {
+    return title.replace(': ', ' ').replace(' - ', ' ')
 }
