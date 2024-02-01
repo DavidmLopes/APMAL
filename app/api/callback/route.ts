8bd88572c18e563a3592a25fe512b51ca337fd39
David Lopes <david.mlopes15@gmail.com>
2024-02-01 21:35:45 +0000

    Hardcode the vercel url

--- app/api/callback/route.ts
+++ app/api/callback/route.ts
@@ -8,7 +8,7 @@ export async function GET(request: NextRequest) {
 
     const baseUrl =
         process.env.NEXT_PUBLIC_VERCEL_URL !== undefined
-            ? 'https://' + process.env.NEXT_PUBLIC_VERCEL_URL
+            ? 'https://apmal.vercel.app/'
             : 'http://localhost:3000'
 
     const form = new FormData()
--- app/authorize/page.tsx
+++ app/authorize/page.tsx
@@ -5,7 +5,7 @@ export default function Authorize() {
 
     const baseUrl =
         process.env.NEXT_PUBLIC_VERCEL_URL !== undefined
-            ? 'https://' + process.env.NEXT_PUBLIC_VERCEL_URL
+            ? 'https://apmal.vercel.app/'
             : 'http://localhost:3000'
 
     redirect(
